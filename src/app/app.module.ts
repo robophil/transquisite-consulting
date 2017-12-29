3bf992faf1dac94dfa6f511ea1041791ec9a8166
Theophilus Omorebee <theo4u@ymail.com>
2017-12-29 17:28:23 +0100

    feat(search-jobs): done with search jobs closes #5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,24 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ContactPage } from '../pages/contact/contact';
 import { Network } from '@ionic-native/network';
 import { FacebookPage } from '../pages/facebook/facebook';
+import { SearchPage } from '../pages/search/search';
+import { UtilProvider } from '../providers/util';
+
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     ContactPage,
-    FacebookPage
+    FacebookPage,
+    SearchPage
   ],
   imports: [
     BrowserModule,
@@ -26,13 +31,16 @@ import { FacebookPage } from '../pages/facebook/facebook';
     MyApp,
     HomePage,
     ContactPage,
-    FacebookPage
+    FacebookPage,
+    SearchPage
   ],
   providers: [
     StatusBar,
     SplashScreen, 
     Network,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    InAppBrowser,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UtilProvider
   ]
 })
 export class AppModule {}
---
+++ src/providers/util.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { InAppBrowser, InAppBrowserEvent } from '@ionic-native/in-app-browser';
+
+/*
+  Generated class for the UtilProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class UtilProvider {
+
+  constructor(private _iab: InAppBrowser) {
+    console.log('Hello UtilProvider Provider');
+  }
+
+  /**
+ * simply open in app browser to load up our webpage
+ */
+  openInAppBrowser(link: string) {
+    let browser = this._iab.create(link, "_blank", "location=yes");
+    browser.on('loadstart').subscribe((event: InAppBrowserEvent) => {
+      console.log("load start event", event);
+    });
+    browser.on('exit').subscribe((event: InAppBrowserEvent) => {
+      console.log("browser closed", event);
+    });
+
+    browser.show();
+  }
+
+}
